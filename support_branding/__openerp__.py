0c8035ee9f75ebf8b452534742b2449972a1e91c
Stefan Rijnhart <stefan@therp.nl>
2012-01-22 14:53:19 +0100

    [ADD] support_branding module

---
+++ support_branding/__openerp__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2012 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    "name": "Support branding",
+    "description": """If you run an OpenERP support company and you support
+customers without an OPW, you can brand the OpenERP instance
+accordingly using this module. This module will replace the unfriendly
+message about the OpenERP instance not being supported with the information
+that your customers need to contact you.
+
+Ths is a web module. You cannot install it through the OpenERP modules
+interface. Instaed, to enable this module set 'active' to True in
+support_branding/__openerp__.py and restart the OpenERP server.
+
+To configure this module, please enter the values for thetwo
+variables 'support_name' and 'support_link' in the file
+support_branding/static/src/js/chrome.js.
+
+This module is compatible with OpenERP 6.1.
+""",
+    "category": "Therp web addons",
+    "version": "1.0",
+    "author": "Therp BV",
+    "website": 'http://therp.nl',
+    "depends": ['web'],
+    "js": ["static/src/js/chrome.js"],
+    "qweb": ["static/src/xml/base.xml"],
+    "css": [],
+    'active': False,
+}
---
+++ support_branding/static/src/js/chrome.js
@@ -0,0 +1,80 @@
+/* 
+
+   Copyright (C) 2012 Therp BV
+   License: GNU AFFERO GENERAL PUBLIC LICENSE
+            Version 3 or any later version
+
+   Usage: if you run an OpenERP support company and you support
+   customers without an OPW, you can brand the OpenERP instance
+   accordingly using this module. Please enter the two variables
+   in the code below, 'support_name' and 'support_link'. They will
+   replace the unfriendly message about the OpenERP instance not
+   being supported.
+           
+ */
+   
+openerp.support_branding = function(openerp) {
+
+    /* Fill in the data of your supporting company */
+
+    var support_name = 'Example Co Ltd.';
+    var support_link = 'http://example.com';
+
+    /* End of configurable settings */
+
+    var QWeb = openerp.web.qweb,
+    _t = openerp.web._t;
+    
+    openerp.web.CrashManager = openerp.web.CrashManager.extend({
+        on_managed_error: function(error) {
+            $('<div>' + QWeb.render(
+                'CrashManagerWarning', {
+                    error: error,
+                    support_name: support_name,
+                    support_link: support_link,
+                }) + '</div>').dialog({
+                title: "OpenERP " + _.str.capitalize(error.type),
+                buttons: [
+                    {text: _t("Ok"), click: function() { $(this).dialog("close"); }}
+            ]
+            });
+        },
+        on_traceback: function(error) {
+            if (openerp.connection.openerp_entreprise) {
+                return this._super(error);
+            }
+            var dialog = new openerp.web.Dialog(this, {
+                title: "OpenERP " + _.str.capitalize(this.error.type),
+                width: '80%',
+                height: '50%',
+                min_width: '800px',
+                min_height: '600px',
+                buttons: [
+                    {text: _t("Ok"), click: function() { $(this).dialog("close"); }}
+                ]
+            }).open();
+            dialog.$element.html(QWeb.render('CrashManagerError', {
+                session: openerp.connection,
+                error: error,
+                support_name: support_name,
+                support_link: support_link,
+            }));
+        },
+    });
+
+    openerp.webclient.session.on_session_valid.add_last(function () {
+        if (!openerp.webclient.session.openerp_entreprise) {
+            openerp.webclient.$element.find('.oe_footer_powered').replaceWith(
+                '<p class="oe_footer_powered">Powered by ' +
+                    '<a href="http://www.openerp.com">OpenERP</a> - ' +
+                    'Supported by <a href="' + support_link + 
+                    '" target="_blank">' + support_name +
+                    '</a></span><p>'
+            );
+            document.title = openerp.web._t(
+                "OpenERP");
+        }
+    });
+};
+
+
