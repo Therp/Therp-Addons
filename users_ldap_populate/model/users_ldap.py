471bfa0af94cfc2bca95a301e16fb0af67c0fff8
Holger Brunn <hbrunn@therp.nl>
2013-10-28 16:23:46 +0100

    [IMP] don't use self.query() to be sure to be stopped if any error occurs

--- users_ldap_populate/model/users_ldap.py
+++ users_ldap_populate/model/users_ldap.py
@@ -20,6 +20,7 @@
 ##############################################################################
 
 import re
+import ldap
 from ldap.filter import filter_format
 from osv import osv, fields
 from openerp import SUPERUSER_ID
@@ -145,9 +146,18 @@ class CompanyLDAP(osv.osv):
     def get_ldap_entry_dicts(self, conf, user_name='*'):
         """
         Execute ldap query as defined in conf
+
+        Don't call self.query because it supresses possible exceptions
         """
         ldap_filter = filter_format(conf['ldap_filter'] % user_name, ())
-        return self.query(conf, ldap_filter)
+        conn = self.connect(conf)
+        conn.simple_bind_s(conf['ldap_binddn'] or '',
+                           conf['ldap_password'] or '')
+        results = conn.search_st(conf['ldap_base'], ldap.SCOPE_SUBTREE,
+                                 ldap_filter, None, timeout=60)
+        conn.unbind()
+
+        return results
 
     def populate_wizard(self, cr, uid, ids, context=None):
         """
