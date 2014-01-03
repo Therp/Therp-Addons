36ded3ad8e394520f0c29189426420bd22a6bdfe
Holger Brunn <hbrunn@therp.nl>
2014-01-03 12:43:41 +0100

    [IMP] use mail_thread.message_{new,update} if applicable [ADD] show attachments on mail.message form if applicable [ADD] group who manages the inbox [ADD] more elaborate description

--- fetchmail_inbox/__openerp__.py
+++ fetchmail_inbox/__openerp__.py
@@ -30,12 +30,32 @@
 
     This module provides the base for this workflow and elementary UI for
     processing.
+
+    Usage
+    -----
+
+    Create a fetchmail configuration and use 'Fetchmail inbox' as object to be
+    created on mail arrival. Be sure to check Advanced/Keep original in order
+    not to lose data in the intermediate step via the inbox.
+
+    Mails fetched from this configuration end up in Email/Fetchmail Inbox,
+    where they can be reviewed and eventually used to create new objects or
+    attached to existing objects.
+
+    Further development
+    -------------------
+
+    This module deals with emails in a very generic way, which is good for
+    flexibility, but bad for usability. Fortunately, it was developed with
+    extensibility in mind so that it is very simple to write extension modules
+    to ease handling emails for specific models in a more user friendly manner.
     """,
     "category" : "Dependency",
     "depends" : [
         'fetchmail',
     ],
     "data" : [
+        "security/res_groups.xml",
         "wizard/fetchmail_inbox_create_wizard.xml",
         "wizard/fetchmail_inbox_attach_existing_wizard.xml",
         "view/mail_message.xml",
