229a1466f85c1f1fe663b436a1bd093fd15a072a
Holger Brunn <hbrunn@therp.nl>
2014-07-22 09:33:53 +0200

    [FIX] return/continue

--- partner_name_dutch/model/res_partner.py
+++ partner_name_dutch/model/res_partner.py
@@ -46,7 +46,7 @@ class res_partner(Model):
         #the trigger
         if hasattr(self, '_display_name_store_triggers'):
             if self._name not in self._display_name_store_triggers:
-                continue
+                return
             self._display_name_store_triggers[self._name][1].extend(
                 ['infix', 'initials'])
             for trigger in self.pool._store_function[self._name]:
