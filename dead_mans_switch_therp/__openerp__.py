48ce93663c428f6c4b65b3610a10503f3689de9c
Holger Brunn <hbrunn@therp.nl>
2015-12-15 10:21:09 +0100

    [FIX] version number from cherry pick

--- dead_mans_switch_therp/__openerp__.py
+++ dead_mans_switch_therp/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Dead man's switch for Therp",
-    "version": "6.1.1.0.0",
+    "version": "7.0.1.0.0",
     "author": "Therp BV",
     "license": "AGPL-3",
     "category": "Monitoring",
