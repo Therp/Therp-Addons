f1176b7f1a3a5558d2e31e269b86267817376cd1
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-03-31 13:40:57 +0200

    [REM] Old backlog model, such model does not exist in project_scrum [ADD] dependency from trp_external_user

---
+++ trp_project_scrum/models/__init__.py
@@ -0,0 +1,2 @@
+from . import res_users
+from . import project_task_work
---
+++ trp_project_scrum/models/project_task_work.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from openerp import _, api, exceptions, fields, models
+
+
+class ProjectTaskWork(models.Model):
+    _inherit = "project.task.work"
+
+    @api.model
+    def get_product_for_collaborator(self):
+        # defaults for therp
+        product_id = self.env['product.product'].browse(
+            self.env['hr.employee']._getEmployeeProduct()
+        )
+        return product_id
+
+    @api.model
+    def get_user_related_details(self, user_id):
+        emp_obj = self.env['hr.employee']
+        user = self.env['res.users'].browse(
+            user_id or self._uid
+        )
+        emp_id = emp_obj.search(
+            [('user_id', '=', user.id)],
+            limit=1
+        )
+        if not emp_id and user.sudo().has_group(
+                'trp_project_scrum.collaborators_followers'):
+            product_id = self.sudo().get_product_for_collaborator()
+            general_account = (
+                product_id.categ_id.property_account_expense_categ.id
+                or product_id.property_account_expense.id
+            )
+            if not general_account:
+                raise exceptions.Warning(_(
+                    'No account chosen for this user, and no other user to'
+                    'copy account from was found, contact admin'))
+            res = {
+                'product_id': product_id.id,
+                'journal_id': self.env['hr.employee']._getAnalyticJournal(),
+                'general_account_id': general_account,
+                'product_uom_id': product_id.uom_id.id
+            }
+            # TODO create default data for collaborator
+            # this should be done if we want separate managment of external
+            # collaborators products and journals for reporting purposes
+            """
+            res= {
+                'product_id' : self.env.ref('collaborator_default_product').id,
+                'journal_id' : self.env.ref('collaborator_default_journal').id,
+                'general_account_id' : self.env.ref(
+                    'collaborator_default_account'
+                ),
+                'product_uom_id' : self.env.ref(
+                    'collaborator_default_product'
+                ).uom_id.id,
+            }
+            """
+            return res
+
+        res = super(ProjectTaskWork, self).get_user_related_details(user_id)
+        return res
