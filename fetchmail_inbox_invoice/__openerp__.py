11fe1e769c04ecc2d66f2f8c5c488f56839f27ba
Stefan Rijnhart <stefan@opener.am>
2017-04-14 18:06:21 +0200

    [RFR] Conventions

--- fetchmail_inbox_invoice/__openerp__.py
+++ fetchmail_inbox_invoice/__openerp__.py
@@ -3,20 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Fetch mail into inbox (invoice specific)",
-    "version": "8.0.1.0",
+    "version": "8.0.1.0.0",
     "author": "Therp BV",
-    "complexity": "normal",
-    "description": """
-Fetch emails from a mail server to attach to existing invoices. The emails are
-kept in a dedicated inbox for a user to review the mails manually.
-
-Usage
-=====
-
-Create a fetchmail configuration for the model "Fetchmail inbox for
-invoices". The mails fetched from this server will be put into
-Accounting / Suppliers / Fetchmail inbox for further processing.
-    """,
     "category": "Accounting & Finance",
     "depends": [
         'account',
@@ -27,15 +15,5 @@ Accounting / Suppliers / Fetchmail inbox for further processing.
         'view/menu.xml',
         'security/ir.model.access.csv',
     ],
-    "js": [
-    ],
-    "css": [
-    ],
-    "qweb": [
-    ],
-    "auto_install": False,
     "installable": True,
-    "external_dependencies": {
-        'python': [],
-    },
 }
