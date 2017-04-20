f5b26f92130ced670de0bdb02298fd9cc5726ac6
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-04-20 17:35:30 +0200

    review fixes

--- trp_project_scrum/__openerp__.py
+++ trp_project_scrum/__openerp__.py
@@ -22,17 +22,8 @@
 
 {
     "name": "Therp specific adaptations for scrum",
-    "description": """
-Use with project_scrum for Odoo 8.0 from
-https://github.com/EL2DE/openerp8-addons.git
-
-- Makes stages visible for non developers
-- Adds a field for technical details, visible for developers
-- Adds some placeholders for clarification
-- Show followers on user stories
-    """,
     "category": "Therp addons",
-    "version": "1.0",
+    "version": "8.0.1.0.1",
     "author": "Therp BV",
     "website": 'http://therp.nl',
     "depends": [
