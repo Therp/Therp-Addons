683189ac3119694f06a9f7f19bf21468c6d330c4
Ronald Portier <ronald@therp.nl>
2017-05-23 13:00:37 +0200

    [FIX] Flake8 issues and style.

--- find_memory_leaks/find_memory_leaks.py
+++ find_memory_leaks/find_memory_leaks.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2013-2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import objgraph
 import pickle
 import gc
 from openerp.osv.orm import TransientModel
 from openerp.osv import fields
+
 
 def sorted_dict_to_str(d):
     return '\n'.join(
@@ -10,9 +14,11 @@ def sorted_dict_to_str(d):
             for name, count in
                 sorted(
                     d.iteritems(),
-                    lambda x, y: -cmp(x[1], y[1]))])
+                    lambda x, y: -cmp(x[1], y[1])
+                )])
 
-class find_memory_leaks(TransientModel):
+
+class FindMemoryLeaks(TransientModel):
     _name = 'find.memory.leaks'
 
     _columns = {
@@ -21,9 +27,6 @@ class find_memory_leaks(TransientModel):
             'typestats_last': fields.text('typestats last'),
             }
 
-    _defaults = {
-        }
-
     def button_typestats(self, cr, uid, ids, context=None):
         current_stats = objgraph.typestats()
         for this in self.browse(cr, uid, ids, context=context):
@@ -31,27 +34,26 @@ class find_memory_leaks(TransientModel):
                 stats = pickle.loads(this.typestats_last)
             else:
                 stats = {}
-
             delta = dict(
-                        filter(lambda x: x[1] != 0,
-                            [(name, current_stats[name] - stats.get(name, 0))
-                                for name in current_stats]))
-
-            this.write(
-                    {
-                        'typestats_last': pickle.dumps(current_stats),
-                        'output': sorted_dict_to_str(delta),
-                    })
+                filter(
+                    lambda x: x[1] != 0,
+                    [(name, current_stats[name] - stats.get(name, 0))
+                        for name in current_stats]))
+            this.write({
+                'typestats_last': pickle.dumps(current_stats),
+                'output': sorted_dict_to_str(delta),
+            })
 
     def button_get_leaking_objects(self, cr, uid, ids, context=None):
         leaking_objects = objgraph.get_leaking_objects()
         for this in self.browse(cr, uid, ids, context=context):
-            this.write(
-                    {'output':
-                        str(len(leaking_objects))+'\n'+
-                        '\n'.join([str(type(o))
-                            for o in leaking_objects])
-                    })
+            this.write({
+                'output':
+                str(len(leaking_objects)) + '\n' +
+                '\n'.join([
+                    str(type(o))
+                    for o in leaking_objects])
+            })
 
     def button_count_leaking_objects(self, cr, uid, ids, context=None):
         leaking_objects = objgraph.get_leaking_objects()
@@ -59,11 +61,11 @@ class find_memory_leaks(TransientModel):
         for o in leaking_objects:
             count[str(type(o))] = count.get(str(type(o)), 0) + 1
         for this in self.browse(cr, uid, ids, context=context):
-            this.write(
-                    {'output':
-                        str(len(leaking_objects))+'\n'+
-                        sorted_dict_to_str(count),
-                    })
+            this.write({
+                'output':
+                    str(len(leaking_objects)) + '\n' +
+                    sorted_dict_to_str(count),
+            })
 
     def button_gc_collect(self, cr, uid, ids, context=None):
         gc.collect()
