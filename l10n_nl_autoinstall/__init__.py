a8bd368e94e2dee198114c0ed76b22a5325002fb
Holger Brunn <hbrunn@therp.nl>
2017-08-25 16:05:43 +0200

    [ADD] suppress installing modules through account's init hook

--- l10n_nl_autoinstall/__init__.py
+++ l10n_nl_autoinstall/__init__.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
 # © 2014-2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import tools
 from . import models
+
+
+def post_init_hook(cr, pool):
+    if tools.config['test_enable']:
+        # in test mode, remove the modules autoinstalled by account's init hook
+        auto_modules = [
+            'account_plaid', 'account_check_printing', 'l10n_generic_coa',
+        ]
+        self.env['ir.module.module'].search([
+            ('name', 'in', auto_modules),
+        ]).button_uninstall()
