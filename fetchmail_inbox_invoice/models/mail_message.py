ee1b6a564e6c714f66aeb62393e8f31c40c94b87
Stefan Rijnhart <stefan@opener.am>
2017-12-26 12:34:35 +0100

    [MIG] fetchmail_inbox_invoice/10.0

--- fetchmail_inbox_invoice/models/mail_message.py
+++ fetchmail_inbox_invoice/models/mail_message.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # Copyright (C) 2014 Therp BV (<http://therp.nl>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp import api, models
+from odoo import api, models
 
 
 class MailMessage(models.Model):
