47e81b88aa171bf46ead0aa9f19f130e1bfb3deb
Ronald Portier <ronald@therp.nl>
2018-02-09 11:00:08 +0100

    [MIG] l10n_nl_autoinstall. More conservative changing of partners.
    Also update to latest conventions.

--- l10n_nl_autoinstall/__init__.py
+++ l10n_nl_autoinstall/__init__.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-# © 2014-2016 Therp BV <http://therp.nl>
+# Copyright 2014-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import api, tools, SUPERUSER_ID
+from odoo import api, tools, SUPERUSER_ID
 from . import models
 
 
 def post_init_hook(cr, pool):
     if tools.config['test_enable']:
         # in test mode, remove the modules autoinstalled by account's init hook
-        auto_modules = [
-            'account_plaid', 'account_check_printing', 'l10n_generic_coa',
-        ]
+        auto_modules = ['account_check_printing', 'l10n_generic_coa']
         env = api.Environment(cr, SUPERUSER_ID, {})
         env['ir.module.module'].search([
             ('name', 'in', auto_modules),
