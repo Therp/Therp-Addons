4d7d5ed140e0056b7e61d48971fc564133b8d862
Holger Brunn <hbrunn@therp.nl>
2018-09-17 19:05:09 +0200

    [MIG] scheduler_error_mailer_therp

--- scheduler_error_mailer_therp/__manifest__.py
+++ scheduler_error_mailer_therp/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2016 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2016-2018 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Configure scheduler_error_mailer for Therp",
-    "version": "8.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Therp BV",
     "license": "AGPL-3",
     "category": "Extra Tools",
@@ -12,7 +12,7 @@
         'scheduler_error_mailer',
     ],
     "data": [
-        "data/email_template.xml",
+        "data/mail_template.xml",
     ],
     "post_init_hook": 'post_init_hook',
 }
