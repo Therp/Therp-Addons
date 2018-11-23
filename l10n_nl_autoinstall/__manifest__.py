6cabe96ddb588ff76e2d6ebe7ccda5519e217702
George Daramouskas <gdaramouskas@therp.nl>
2018-11-23 13:29:26 +0100

    [MIG] l10n_nl_autoinstall: Migration to 11.0

--- l10n_nl_autoinstall/__manifest__.py
+++ l10n_nl_autoinstall/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Dutch chart of accounts - autoinstall",
-    "version": "10.0.1.0.1",
-    "author": "Therp BV",
+    "version": "11.0.0.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
     "category": 'Accounting & Finance',
     'website': 'http://therp.nl',
     'depends': ['l10n_nl'],
