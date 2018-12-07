89f0e0c55a5e7bfbeb1129f1f708b787b0305c91
George Daramouskas <gdaramouskas@therp.nl>
2018-12-07 11:28:31 +0100

    fixup! [PORT] Port trp_project_scrum

--- trp_project_scrum/tests/test_trp_project_scrum.py
+++ trp_project_scrum/tests/test_trp_project_scrum.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-import random
-import string
 from odoo.tests.common import SingleTransactionCase
 from odoo.exceptions import AccessError
 
