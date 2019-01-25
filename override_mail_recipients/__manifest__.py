fdfc1ae98df456799d2fac82c3164034b0a328e1
Ronald Portier <ronald@therp.nl>
2019-01-25 18:17:13 +0100

    [IMP] [no task] Improve override_mail_recipients.
    1. No more silent discarding of messages. Took me half a day to find out why
    my messages disappeared in a black hole, with only a misleading log entry on
    invalid recipients.
    2. Give users the possibility to disable the recipient override.
    3. Refactored the code to make it more clean and improve possibility
    to test.

--- override_mail_recipients/__manifest__.py
+++ override_mail_recipients/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-2018 Therp BV <https://therp.nl>.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2015-2019 Therp BV <https://therp.nl>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Override mail recipients",
-    "version": "11.0.0.0.0",
+    "version": "11.0.0.1.0",
     "author": "Therp BV",
     "license": "AGPL-3",
     "category": "Tools",
--- override_mail_recipients/models/ir_mail_server.py
+++ override_mail_recipients/models/ir_mail_server.py
@@ -1,35 +1,58 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-2018 Therp BV <https://therp.nl>.
+# Copyright 2015-2019 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, models
 from email.utils import COMMASPACE
+
+from odoo import api, models
 from odoo.addons.base.ir.ir_mail_server import extract_rfc2822_addresses
 
 
 class IrMailServer(models.Model):
+    # pylint: disable=too-few-public-methods
     _inherit = 'ir.mail_server'
+
+    @api.model
+    def patch_message(self, message):
+        """Override message recipients.
+
+        Result is dependent on what is specified in the configuration:
+        1. Message is not changed (no configuration or 'disabled');
+        2. Message recipients are replaced;
+        3. There is no valid recipient in the override configuration.
+
+        If there is no valid recipient, an Exception will be thrown. This
+        will be an AssertionException, in line with what is done by standard
+        Odoo. Most likely this message will be catched and an appropiate
+        reason for non delivery registered.
+        """
+        override_email = self.env.ref(
+            'override_mail_recipients.override_email_to').value
+        if not override_email or override_email == 'disable':
+            return
+        actual_recipients = extract_rfc2822_addresses(override_email)
+        assert actual_recipients, 'No valid override_email_to'
+        for field in ['to', 'cc', 'bcc']:
+            if not message[field]:
+                continue
+            original = COMMASPACE.join(message.get_all(field, []))
+            not_so_original = original.replace('\\', '').replace(
+                '"', '\\"').replace('<', '[').replace('>', ']').replace(
+                    '@', '(at)')
+            del message[field]
+            message[field] = COMMASPACE.join(
+                '"%s" <%s>' % (not_so_original, email)
+                for email in actual_recipients)
 
     @api.model
     def send_email(
             self, message, mail_server_id=None, smtp_server=None,
             smtp_port=None, smtp_user=None, smtp_password=None,
             smtp_encryption=None, smtp_debug=False, smtp_session=None):
-        override_email = self.env.ref(
-            'override_mail_recipients.override_email_to').value
-        if override_email:
-            for field in ['to', 'cc', 'bcc']:
-                if not message[field]:
-                    continue
-                original = COMMASPACE.join(message.get_all(field, []))
-                del message[field]
-                message[field] = COMMASPACE.join(
-                    '"%s" <%s>' % (
-                        original.replace('\\', '').replace('"', '\\"')
-                        .replace('<', '[').replace('>', ']')
-                        .replace('@', '(at)'),
-                        email
-                    )
-                    for email in extract_rfc2822_addresses(override_email))
+        """Override email recipients if requested, then send mail.
+
+        Or throw Exception when no valid recipients.
+        """
+        # pylint: disable=too-many-arguments
+        self.patch_message(message)
         return super(IrMailServer, self).send_email(
             message, mail_server_id=mail_server_id,
             smtp_server=smtp_server, smtp_port=smtp_port, smtp_user=smtp_user,
