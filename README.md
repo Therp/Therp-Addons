1a7cff648a3a5e3dd776877c8edf1f77fb20dc0b
George Daramouskas <gdaramouskas@therp.nl>
2019-01-29 11:57:59 +0100

    [FIX] Extra changes on trp_project_scrum

---
+++ README.md
@@ -0,0 +1,6 @@
+Addons for Odoo by Therp BV
+===========================
+
+This repository contains addons that don't fit into any of the OCA projects.
+
+For commercial support, go to https://therp.nl
--- trp_project_scrum/__manifest__.py
+++ trp_project_scrum/__manifest__.py
@@ -6,18 +6,19 @@
     "category": "Therp addons",
     "version": "10.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Therp BV,Odoo Community Association (OCA)",
+    "author": "Therp BV",
     "website": 'https://therp.nl',
     "depends": [
         'trp_external_user',
         'project_scrum',
         'hr',
-        'document_page',
+        'hr_timesheet',
     ],
     "data": [
         'security/res_groups.xml',
-        'security/ir_rules_collaborators_followers.xml',
+        'security/ir_rules.xml',
         'security/ir.model.access.csv',
+        'views/menus.xml',
         ],
     "demo": [
         'demo/res_users.xml',
