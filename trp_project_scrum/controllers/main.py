6b27b0875f94246fc1e6fe007c106b727cd24d8d
George Daramouskas <gdaramouskas@therp.nl>
2019-04-05 10:43:18 +0200

    fixup! fixup! [FIX] Extra changes on trp_project_scrum

---
+++ trp_project_scrum/controllers/main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import http
+from odoo.http import request
+from odoo.addons.website.controllers import main
+
+
+class Website(main.Website):
+
+    @http.route(website=True, auth="public")
+    def web_login(self, redirect=None, *args, **kwargs):
+        response = super(Website, self).web_login(
+            redirect=redirect, *args, **kwargs)
+        if not redirect and request.params['login_success']:
+            if request.env['res.users'].browse(request.uid).has_group(
+                    'trp_project_scrum.external_scrum_users'):
+                return http.redirect_with_hash(
+                    '/web?' + request.httprequest.query_string)
+        return response
