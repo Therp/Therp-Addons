d165d3d94e7ae2868ea0be9edc93699816078987
Ronald Portier <ronald@therp.nl>
2019-10-29 13:13:36 +0100

    [FIX] override_mail_recipients. Replace assert by info message.

--- override_mail_recipients/tests/test_ir_mail_server.py
+++ override_mail_recipients/tests/test_ir_mail_server.py
@@ -19,28 +19,34 @@ class TestIrMailServer(SingleTransactionCase):
             email_bcc=['bcc@example.com'])
 
     def test_valid_override(self):
-        self.env.ref(
-            'override_mail_recipients.override_email_to'
-        ).value = 't@test.com'
-        self.env['ir.mail_server'].patch_message(self.message)
+        self._set_configuration('t@test.com')
+        self._patch_message(self.message)
         self.assertEquals(
             self.message['to'], '"to(at)example.com" <t@test.com>')
         self.assertEquals(
             self.message['cc'], '"cc(at)example.com" <t@test.com>')
 
     def test_invalid_override(self):
-        self.env.ref(
-            'override_mail_recipients.override_email_to'
-        ).value = 'not going to work'
+        self._set_configuration('not going to work')
         with self.assertRaises(AssertionError):
-            self.env['ir.mail_server'].patch_message(self.message)
+            self._patch_message(self.message)
 
     def test_disable_override(self):
-        self.env.ref(
-            'override_mail_recipients.override_email_to'
-        ).value = 'disable'
-        self.env['ir.mail_server'].patch_message(self.message)
+        self._set_configuration('disable')
+        self._patch_message(self.message)
         self.assertEquals(
             self.message['to'], 'to@example.com')
         self.assertEquals(
             self.message['cc'], 'cc@example.com')
+
+    def _set_configuration(self, value):
+        """Set override mail configuration."""
+        config_model = self.env['ir.config_parameter']
+        config_model.set_param(
+            'override_mail_recipients.override_to', value)
+
+    def _patch_message(self, message):
+        """Let mail server patch message, with test_override in context."""
+        server_model = self.env['ir.mail_server'].with_context(
+            test_override_mail=True)
+        server_model.patch_message(message)
