5b8ae23a4df21ee161b8e943ae487ee8a89b041c
KKamaa <kevkamaa96@gmail.com>
2021-08-13 09:17:11 +0300

    [FIX] OCA format, fix errors, tests

--- trp_external_user/__manifest__.py
+++ trp_external_user/__manifest__.py
@@ -1,24 +1,21 @@
+# Copyright 2013-2018 Therp BV <https://therp.nl>
+# Copyright 2018-2021 Sunflower IT <https://www.sunflowerweb.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 {
     "name": "External users",
     "summary": "Allow external users on your system",
     "version": "13.0.1.0.0",
     "author": "Therp BV, Sunflower IT",
-    "category": 'External users',
-    'website': 'http://therp.nl',
-    'depends': [
-        'portal',
+    "category": "External users",
+    "website": "https://therp.nl",
+    "depends": ["base", "portal"],
+    "demo": ["demo/res_users.xml"],
+    "data": [
+        "data/ir_module_category.xml",
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule.xml",
+        "views/res_users.xml",
     ],
-    'demo': [
-        "demo/res_users.xml",
-    ],
-    'data': [
-        'data/ir_module_category.xml',
-        'security/res_groups.xml',
-        'security/ir.model.access.csv',
-        'security/ir_rule.xml',
-        'views/res_users.xml',
-    ],
-    'license': 'AGPL-3',
+    "license": "AGPL-3",
 }
--- trp_external_user/models/res_users.py
+++ trp_external_user/models/res_users.py
@@ -1,33 +1,41 @@
+# Copyright 2013-2018 Therp BV <https://therp.nl>
+# Copyright 2018-2021 Sunflower IT <https://www.sunflowerweb.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from odoo import api, fields, models
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     @api.model
     def _default_groups(self):
         """Disable default assignment of group 'users' and 'partner manager'"""
-        filter_groups = self.env.ref('base.group_user') + self.env.ref(
-            'base.group_partner_manager'
+        filter_groups = self.env.ref("base.group_user") + self.env.ref(
+            "base.group_partner_manager"
         )
-        return super(ResUsers, self)._default_groups().filtered(
-            lambda x: not x & filter_groups
+        return (
+            super(ResUsers, self)
+            ._default_groups()
+            .filtered(lambda x: not x & filter_groups)
         )
 
-    @api.depends('groups_id')
+    @api.depends("groups_id")
     def _compute_is_external_user(self):
         for this in self:
-            this.is_external_user = self.env['ir.model.access'].sudo(this)\
-                .check_groups('trp_external_user.group_external_user')
+            this.is_external_user = (
+                self.env["ir.model.access"]
+                .with_user(this)
+                .check_groups("trp_external_user.group_external_user")
+            )
 
     external_user_partner_ids = fields.Many2many(
-        'res.partner', 'trp_external_user_partner_id_rel',
-        'user_id', 'partner_id',
-        'External access to related partners',
+        "res.partner",
+        "trp_external_user_partner_id_rel",
+        "user_id",
+        "partner_id",
+        "External access to related partners",
     )
     is_external_user = fields.Boolean(
-        'Is external user', compute='_compute_is_external_user',
+        "Is external user", compute="_compute_is_external_user"
     )
     groups_id = fields.Many2many(default=lambda self: self._default_groups())
