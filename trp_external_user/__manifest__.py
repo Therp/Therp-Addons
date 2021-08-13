f6f10018fcafa6075de5ef0204e03e0d8a168432
KKamaa <kevkamaa96@gmail.com>
2021-08-13 09:24:41 +0300

    [FIX] rm copyright

--- trp_external_user/__manifest__.py
+++ trp_external_user/__manifest__.py
@@ -1,5 +1,3 @@
-# Copyright 2013-2018 Therp BV <https://therp.nl>
-# Copyright 2018-2021 Sunflower IT <https://www.sunflowerweb.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "External users",
