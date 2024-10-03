96515be700d377bbf87e6cc4d61573cad1f8ab34
Ronald Portier <ronald@therp.nl>
2024-10-03 14:49:22 +0200

    [FIX] override_mail_recipients: no more view_type in action

--- override_mail_recipients/__manifest__.py
+++ override_mail_recipients/__manifest__.py
@@ -1,9 +1,10 @@
-# Copyright 2015-2019 Therp BV <https://therp.nl>.
+# Copyright 2015-2024 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Override mail recipients",
-    "version": "12.0.0.2.0",
+    "version": "16.0.1.0.0",
     "author": "Therp BV",
+    "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "category": "Tools",
     "summary": "Allows you to override all mail recipients for testing purposes",
